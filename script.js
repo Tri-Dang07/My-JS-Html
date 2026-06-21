a39982bd4e9b23b7b15000a7b605faf52e5947eb
Tri-Dang07 <pox5554@gmail.com>
2026-06-21 12:18:46 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// MyJS/js/script.js
-const btnExternal = document.getElementById('btnExternal');
-btnExternal.addEventListener('click', function() {
-    alert('Bạn vừa bấm vào nút External!');
-});
